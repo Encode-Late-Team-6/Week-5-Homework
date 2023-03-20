d873475ba642126a8d34732f6726472d0b477dc1
cuegod <hengxingtianxia@gmail.com>
2023-03-20 20:57:41 +0800

    week 5 hw

---
+++ backend/hardhat.config.ts
@@ -0,0 +1,8 @@
+import { HardhatUserConfig } from "backend/config";
+import "@nomicfoundation/backend-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.18",
+};
+
+export default config;
